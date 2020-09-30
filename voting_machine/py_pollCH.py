bfa405a3039e4385810daad8eb0bda961ce08d33
Chanafin <58449415+chanafin@users.noreply.github.com>
2020-09-30 14:55:35 -0400

    Final Vote Machine

--- voting_machine/py_pollCH.py
+++ voting_machine/py_pollCH.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 15 14:47:53 2020
-
-@author: chris
-"""
-
 import os
 import csv
 
+"Create empty vote lists"
 voterid = []
 Khanvote = []
 Correyvote = []
@@ -15,17 +10,20 @@ Livote = []
 Otooleyvote = []
 
 #%%
+
+"Join, Open and Read the CSV"
 closedfile = os.path.join('election_data.csv')
 with open(closedfile, 'r') as openfile:
     pypoll = csv.reader(openfile, delimiter = ',')
-    next(pypoll)
-    
+    next(pypoll) 
+    "Loop through the CSV"
     for i in pypoll:
         voterid_col = i[0]
         candidate_col = i[2]
-          
-        voterid.append(voterid_col)
         
+        "Append the each vote id to the vote ID list"
+        voterid.append(voterid_col)
+        "Append each vote for a specific candidate to the candidate's list"
         if candidate_col == 'Khan':
             Khanvote.append(candidate_col)
         if candidate_col == 'Correy':
@@ -35,17 +33,20 @@ with open(closedfile, 'r') as openfile:
         if candidate_col == "O'Tooley":
             Otooleyvote.append(candidate_col)
 #%%
-
+"Determine the lengh of each list"
 total_votes = len(voterid)
 Khan = len(Khanvote)
 Correy = len(Correyvote)
 Li = len(Livote)
 Otooley = len(Otooleyvote)
 #%%
+"Put each list into the master list and determine which list is the longest"
 winners_circle = [Khan, Correy, Li, Otooley]
 winner = max(winners_circle)
 winner_name = ' ' 
 
+
+"Assign the winning candidate's name as the winner"
 if winner == Khan:
     winner_name = "Khan"
 elif winner == Correy:
@@ -54,13 +55,15 @@ elif winner == Li:
     winner_name = "Li"
 else: winner_name = "O'Tooley"
 #%%
-
+"Determine Vote Share"
 khanpct = Khan / total_votes
 correypct = Correy / total_votes
 lipct = Li / total_votes
 otooleypct = Otooley / total_votes
 
 #%%
+"Format and output the results"
+
 output = os.path.join('poll.txt')
 with open(output, "w") as txt_file:
     election_results = (
