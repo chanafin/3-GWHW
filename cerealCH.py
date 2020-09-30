18fa39ecbc4d0253ad0b32868b6dda0349bd48b5
Chanafin <58449415+chanafin@users.noreply.github.com>
2020-09-30 11:21:58 -0400

    Cereal Edits

--- cerealCH.py
+++ cerealCH.py
@@ -1,24 +1,22 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 15 18:39:13 2020
 
-@author: chris
-"""
-
+"Import Dependencies"
 import os
 import csv
-    
+
+"Join, Open and Read the CSV" 
 closed_file = os.path.join('cereal.csv')
 with open(closed_file, 'r') as open_file:
     
     cereal_reader = csv.reader(open_file)
     next(cereal_reader, None)
-    
-    
+    "Loop through cereal_reader, identify the columns you wish to analyze"  
     for i in cereal_reader:
         fiber_count = i[7]
         cereal_name = i[0]
+        "Run an if statement based on the condition you wish to explore" 
         if float(fiber_count) >= 5:
+            "Use an f-string to include the defined variables within your loop"
             print(f'{cereal_name} has {fiber_count} grams of fiber')
     
     
